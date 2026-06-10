e36a894de637087cf9440a54109d545ae11778f7
Valclemir Soares Dos Reis <135272998+ValclemirS@users.noreply.github.com>
2026-06-10 19:08:35 -0300

    third commit

--- src/models/config.ts
+++ src/models/config.ts
@@ -1,1 +1,1 @@
-export const KIRVANO_URL = "https://pay.kirvano.com/SEU-LINK-DE-CHECKOUT"; // ⬅️ TROQUE AQUI
+export const KIRVANO_URL = "https://pay.kirvano.com/17e63798-1184-46fb-b6c2-62c70a58023b"; // ⬅️ TROQUE AQUI
